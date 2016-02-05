8eeb730953e5818bf1bacde8dfd693fa7d6f8eb0
Michael Veilleux <TheGodlyOne95@gmail.com>
2016-02-05 18:25:58 -0500

    YEAH GOT IT

---
+++ app/src/main/java/com/band/guy/kool/connect4/GestionnaireJoueurs.java
@@ -0,0 +1,76 @@
+package com.band.guy.kool.connect4;/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Dominic
+ */
+public class GestionnaireJoueurs {
+
+    private static GestionnaireJoueurs instance = null;
+
+    private int joueurActif;
+    
+    private final ArrayList<Joueur> joueurs;
+
+    protected GestionnaireJoueurs() {
+        joueurs = new ArrayList<>();
+    }
+
+    public static GestionnaireJoueurs avoirInstance() {
+        if (instance == null) {
+            instance = new GestionnaireJoueurs();
+        }
+
+        return instance;
+    }
+
+    public void ajouterJoueur(String nom, char nomCourt) {
+        if(avoirJoueurAvecNomCourt(nomCourt) == null) {
+            joueurs.add(new Joueur(nom, nomCourt, false));
+        }
+    }
+    
+    public void ajouterOrdinateur(String nom, char nomCourt) {
+        if(avoirJoueurAvecNomCourt(nomCourt) == null) {
+            joueurs.add(new Joueur(nom, nomCourt, true));
+        }
+    }
+    
+    public int nombreJoueurs() {
+        return this.joueurs.size();
+    }
+    
+    public void prochainJoueur() {
+        if(++this.joueurActif >= this.joueurs.size()) {
+            this.joueurActif = 0;
+        }
+    }
+    
+    public Joueur avoirJoueurActif() {
+        return joueurs.get(this.joueurActif);
+    }
+    
+    public void changerJoueurActif(char nomCourt) {
+        for(int i = 0; i < this.joueurs.size(); i++) {
+            if(this.joueurs.get(i).avoirNomCourt() == nomCourt) {
+                this.joueurActif = i;
+            }
+        }
+    }
+    
+    public Joueur avoirJoueurAvecNomCourt(char nomCourt) {
+        for(Joueur joueur : this.joueurs) {
+            if(nomCourt == joueur.avoirNomCourt()) {
+                return joueur;
+            }
+        }
+        return null;
+    }
+}
---
+++ app/src/main/java/com/band/guy/kool/connect4/IActionsPuissance4.java
@@ -0,0 +1,11 @@
+package com.band.guy.kool.connect4;/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+public interface IActionsPuissance4 {
+    public Joueur avoirJoueur(int ligne, int colonne);
+    public int jouer(int colonne);
+}
